81342e7ada8c6498f5a7370f18c0d113be697502
Rullian Siqueira da Mata <rullianjbt@gmail.com>
2021-03-11 14:07:34 -0300

    Implementando Persistência, Bean Validation e Exception Handler

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,30 +1,86 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+import com.algaworks.osworks.domain.service.CadastroClienteService;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-	@GetMapping("/clientes")
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private CadastroClienteService cadastroCliente;
+	
+	@GetMapping
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João das Couves teste");
-		cliente1.setTelefone("34 -99999-1111");
-		cliente1.setEmail("joaodascouves@algaworks.com");
+		return clienteRepository.findAll();
+		/*
+		return clienteRepository.findByNome("João da Silva")
+		return clienteRepository.findByNomeContaining("Si");
+		*/
+	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("11 777777-1111");
-		cliente2.setEmail("mariadasilva@algaworks.com");
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
 		
-		return Arrays.asList(cliente1, cliente2);
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+		return cadastroCliente.salvar(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId, 
+			@RequestBody Cliente cliente){
+		
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(clienteId);
+		cliente = cadastroCliente.salvar(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+		
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cadastroCliente.excluir(clienteId);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
